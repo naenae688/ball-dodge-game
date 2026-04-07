c9ab02e6476f523c798aa8565b22383cd464e90e
hendricknkuba <imhendrick16@gmail.com>
2026-04-07 10:34:16 -0700

    feat: add game constants for player and ball dimensions

---
+++ constants/gameConstants.js
@@ -0,0 +1,6 @@
+export const PLAYER_WIDTH = 40;
+export const PLAYER_HEIGHT = 40;
+export const PLAYER_MOVE_STEP = 30;
+export const BALL_RADIUS = 18;
+export const BALL_COUNT = 4;
+export const CONTROL_HEIGHT = 96;
